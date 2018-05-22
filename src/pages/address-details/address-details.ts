82815dd4ffb3143846778571e1f92c2068d253e8
Rohit prasad <rohit.prasad@brainvire.com>
2018-05-22 18:26:40 +0530

    adress prefill data

--- src/pages/address-details/address-details.ts
+++ src/pages/address-details/address-details.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
-
+// import { FormGroup, FormControl } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -21,11 +21,51 @@ export class AddressDetailsPage {
   public selectOptions3 = {
     title: 'Select Country'
   };
+  
+  public location = [
+    {
+      'placeName' : 'Mumbai',
+      'stateName' : 'Maharashtra',
+      'countryName' : 'India',
+      'pinCode' : '285643'
+    },
+    {
+      'placeName' : 'Kanpur',
+      'stateName' : 'Uttar Pradesh',
+      'countryName' : 'India',
+      'pinCode' : '456734'
+    },
+    {
+      'placeName' : 'Bavla',
+      'stateName' : 'Gujarat',
+      'countryName' : 'India',
+      'pinCode' : '382220'
+    }
+  ];
+  public place: any;
+  public state: any;
+  public country:any;
+  public pincode: any;
+  
   constructor(
     public navCtrl: NavController,
+    public _cdr: ChangeDetectorRef,
     public modalCtrl: ModalController, 
     public navParams: NavParams) {
-  }
+      
+    }
+    
+    onPlaceChange(): void{
+      
+      for(let obj of this.location){
+        if(obj.placeName === this.place){
+          this.state = obj.stateName;
+          this.country = obj.countryName;
+          this.pincode = obj.pinCode;
+        }
+      }
+        this._cdr.detectChanges();
+    }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddressDetailsPage');
