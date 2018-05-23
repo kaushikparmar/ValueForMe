480bebfca2be84105ee8eccfdb6c397f2320ebd7
Akashdeep <akashdeep.sharma@brainvire.com>
2018-05-23 16:08:28 +0530

    kyc changes

--- src/providers/data/data.ts
+++ src/providers/data/data.ts
@@ -1,57 +1,83 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
 
 @Injectable()
 export class DataProvider {
-  familyName: any;
-  panDetails: any;
-  constructor(public http: HttpClient) {
-    this.familyName = [
-      {name : 'Rohit Prasad'},
-      {name : 'Pradip Malpute'},
-      {name : 'Ravindra Salukhe'},
-      {name : 'Kaushik Parmar'},
-      {name : 'Satish Gaurav'}
-    ]
-    this.panDetails = [
-      {
-        value : 'ABCDEF123456',
-        name : 'Rohit Prasad',
-        title : "Mr."
+  public familyName: any;
+  public usersList: any = [
+    {
+      'name': 'Rohit Prasad',
+      'title': "Mr.",
+      'familyName': 'Rohit Family',
+      'mobile': '9022905438',
+      'contact': '0265456743',
+      'pan_no': 'ABCDEF123456',
+      'aadhar_no': 'ABCDEF123456',
+      'email': 'rohit.prasad@gmail.com',
+      'isNRI': false,
+      'isNewMember': false
     },
-      {
-        value : 'ABCDEF456789',
-        name : 'Pradip Malpute',
-        title : "Mr."
-      },
-      {
-        value : 'ABCDEF546454',
-        name : 'Ravindra Salukhe',
-        title : "Mr."
-      },
-      {
-        value : 'ABCDEF491020',
-        name : 'Kaushik Parmar',
-        title : "Mr."
-      },
-      {
-        value : 'ABCDEF491023',
-        name : 'Satish Gaurav',
-        title : "Mr."
-      }
+    {
+      'name': 'Pradip Malpute',
+      'title': "Mr.",
+      'familyName': 'Pradip Family',
+      'mobile': '9022904438',
+      'contact': '0265456743',
+      'pan_no': 'ABCDEF456789',
+      'aadhar_no': 'ABCDEF123456',
+      'email': 'pradip.malpute@gmail.com',
+      'isNRI': false,
+      'isNewMember': false
+    },
+    {
+      'familyName': 'Ravindra Family',
+      'title': "Mr.",
+      'mobile': '9022105438',
+      'contact': '0265456743',
+      'pan_no': 'ABCDEF546454',
+      'aadhar_no': 'ABCDEF123456',
+      'email': 'ravindra.salukhe@gmail.com',
+      'name': 'Ravindra Salukhe',
+      'isNRI': false,
+      'isNewMember': false
+    },
+    {
+      'name': 'Kaushik Parmar',
+      'title': "Mr.",
+      'familyName': 'Kaushik Family',
+      'mobile': '9022305438',
+      'contact': '0265456743',
+      'pan_no': 'ABCDEF491020',
+      'aadhar_no': 'ABCDEF123456',
+      'email': 'kaushik.parmar@gmail.com',
+      'isNRI': false,
+      'isNewMember': false
+    },
+    {
+      'familyName': 'Satish Family',
+      'title': "Mr.",
+      'mobile': '9025405438',
+      'contact': '0265456743',
+      'pan_no': 'ABCDEF491023',
+      'aadhar_no': 'ABCDEF123456',
+      'email': 'satish.gaurav@gmail.com',
+      'name': 'Satish Gaurav',
+      'isNRI': false,
+      'isNewMember': false
+    }
+  ];
 
-    ]
+  constructor() {
   }
-  filterName(searchName) {
-    return this.familyName.filter((names) => {
-      return names.name.toLowerCase().indexOf(searchName.toLowerCase()) > -1;
+
+  public filterName(searchName) {
+    return this.usersList.filter((obj) => {
+      return obj.familyName.toLowerCase().indexOf(searchName.toLowerCase()) > -1;
     })
   }
 
-  filterPanData(searchPan) {
-    return this.panDetails.filter((panNo) => {
-      return panNo.value.toLowerCase().indexOf(searchPan.toLowerCase()) > -1;
+  public filterPanData(searchPan) {
+    return this.usersList.filter((panNo) => {
+      return panNo.value.toLowerCase() === searchPan.toLowerCase();
     })
   }
 
